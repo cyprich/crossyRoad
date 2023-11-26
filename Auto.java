24775547d0ded29a24016c1cf4fba155155d2227
Peter Cyprich <cypooriginal@gmail.com>
2023-11-26 11:11:32 +0100

    checkpoint 1

---
+++ Auto.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Auto here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Auto {
+    /**
+     * Constructor for objects of class Auto
+     */
+    public Auto() {
+        // initialise instance variables
+    }
+    
+    public void zobraz(){}
+    
+    public void skry(){}
+}
