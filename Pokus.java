8773affe7a678c643d13b3a6dc175534aed17131
Peter Cyprich <cypooriginal@gmail.com>
2024-01-06 01:41:24 +0100

    Delete Pokus.java

--- Pokus.java
+++
@@ -1,8 +0,0 @@
-public class Pokus {
-    public static void main(String[] args) {
-        for (TypPrekazky x: TypPrekazky.values()) {
-            System.out.println(x.getCesta());
-        }
-        System.out.println(TypPrekazky.values().length);
-    }
-}
